5d8a66e21e6d6a38f2e6076f30145b07c168a694
Marcos Maldonado <markmaldonado406@gmail.com>
2021-04-15 22:10:05 -0500

    fundamentosjs

---
+++ controlesdeflujo.js
@@ -0,0 +1,94 @@
+/*var i = 17
+var p = "17"
+var a = 15
+var t = 17
+var s = 17
+var f = 17
+var q = 17
+var g = 17
+var k = true
+var d = null
+
+if (i == 18) {
+    console.log("Si son numeros iguales.")
+} else { console.log("No son numeros iguales") }
+
+if (a != 18) {
+    console.log("No son numeros iguales.")
+} else { console.log("Si son numeros iguales") }
+
+if (p !== 18) {
+    console.log("No son iguales.")
+} else { console.log("Si son iguales") }
+
+if (t === 18) {
+    console.log("son iguales.")
+} else { console.log("No son numeros iguales") }
+
+if (s < 18) {
+    console.log("17 si es menor")
+} else { console.log("17 no es menor") }
+
+if (f > 18) {
+    console.log("17 si es menor")
+} else { console.log("17 no es menor") }
+
+if (q >= 18) {
+    console.log("el valor es mayor que 18")
+} else { console.log("17es menor que 18") }
+
+if (g <= 18) {
+    console.log("17 es menor que 18")
+} else { console.log("el valor es mayor a 18") }
+
+
+//POR TIPO DE DATO
+
+if (i === p) {
+    console.log("truethie")
+} else { console.log("falsie") }
+
+if (i !== 18) {
+    console.log("truethie")
+} else { console.log("falsie") }
+if (i == 18) {
+    console.log("truethie")
+} else { console.log("falsie") }
+if (i == 18) {
+    console.log("truethie")
+} else { console.log("falsie") }
+if (i == 18) {
+    console.log("truethie")
+} else { console.log("falsie") }
+
+*/
+
+
+
+//Ternarios
+//var 1==1 ? 1 : 0
+
+//switch
+var color = "rojo"
+switch (color) {
+    case "rojo":
+        console.log("el color de la ropa es rojo")
+        break
+    case "azul":
+        console.log("el color de la ropa es azul")
+        break
+    case "verde":
+        console.log("el color de la ropa es verdel")
+        break
+    case "amarillo":
+        console.log("el color de la ropa es amarillo")
+        break
+    case "rosa":
+        console.log("el color de la ropa es rosa")
+        break
+    case "negro":
+        console.log("el color de la ropa es negro")
+        break
+    default:
+        console.log("no se ha definido el color")
+}
---
+++ javascript.js
@@ -0,0 +1,25 @@
+var h = "1"
+var o = "2"
+var l = "3"
+var a = "4"
+    //RESTA
+console.log(h - l) // -2
+console.log(a - o) //  2
+    //SUMA
+console.log(l + o) //  5
+console.log(o + a) //  6
+    //DIVISION
+console.log(l / h) //  3
+console.log(a / o) //  2
+    //MULTIPLICACION
+console.log(l * o) //  6
+console.log(a * l) //  12
+    //MODULO
+console.log(l / h) //  3
+console.log(a / o) //  2
+    //INCREMENTO/DECREMENTO
+h++;
+console.log(h)
+
+a--;
+console.log(a)
