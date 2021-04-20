b19adc58d33afc4269fd52689255069f77f01d48
Marcos Maldonado <markmaldonado406@gmail.com>
2021-04-20 18:48:09 -0500

    redoing variables lost doc

---
+++ variables.js
@@ -0,0 +1,12 @@
+//variables
+var nombre = "marcos"
+var apellido = "maldonado"
+var secNombre = "Josafat"
+
+
+var age = 28
+var houses = 1
+var callphones = 2
+
+var havecat = true
+var havedog = false
